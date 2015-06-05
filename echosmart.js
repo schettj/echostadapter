fbcf15834379c1befaedec938290abeafd10587e
schettj <schettj@users.noreply.github.com>
2015-06-05 16:00:31 -0700

    comment error

--- echosmart.js
+++ echosmart.js
@@ -360,10 +360,10 @@ ath.floor(255 * lights[i].state.level / 100) + ',"hue": 13088,"sat": 212, "xy": 
                             var i = parseInt(a4) - 1;
 				if (!lights[i].state.hasOwnProperty("level")) lights[i].state["level"]=99;
                             //console.log('"state": {"on": ' + ((lights[i].state.switch == "on") ? "true" : "false") + ',"bri":' + Math.floor(255 * lights[i].
-state.level / 100) + ',"hue": 13088,"sat": 212, "xy": [0.5128,0.4147],"ct": 467,\
-        "alert": "none","effect": "none","colormode": "xy", "reachable": true},"type": "Extended color light",\
-        "name": "'+ lights[i].label + '", "modelid": "LCT001","swversion": "66009461", "pointsymbol": {\
-            "1": "none", "2": "none", "3": "none","4": "none", "5": "none","6": "none","7": "none","8": "none" }');
+//state.level / 100) + ',"hue": 13088,"sat": 212, "xy": [0.5128,0.4147],"ct": 467,\
+//        "alert": "none","effect": "none","colormode": "xy", "reachable": true},"type": "Extended color light",\
+//        "name": "'+ lights[i].label + '", "modelid": "LCT001","swversion": "66009461", "pointsymbol": {\
+//            "1": "none", "2": "none", "3": "none","4": "none", "5": "none","6": "none","7": "none","8": "none" }');
                             response.write('"state": {"on": ' + ((lights[i].state.switch == "on") ? "true" : "false") + ',"bri":' + Math.floor(255 * lights[i]
 .state.level / 100) + ',"hue": 13088,"sat": 212, "xy": [0.5128,0.4147],"ct": 467,\
         "alert": "none","effect": "none","colormode": "xy", "reachable": true},"type": "Extended color light",\
